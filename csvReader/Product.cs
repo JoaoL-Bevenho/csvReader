c8f8106558ab95620d107b8d876d8298bc414bc1
JoaoL-Bevenho <40764113+JoaoL-Bevenho@users.noreply.github.com>
2023-02-10 22:15:34 -0300

    Add files via upload

---
+++ csvReader/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvReader
+{
+    public class Product
+    {
+
+        private string name;
+        private string price;
+        private string brand;
+        private string weight;
+
+
+        public string Name { get => name; set => name = value; }
+        public string Price { get => price; set => price = value; }
+        public string Brand { get => brand; set => brand = value; }
+        public string Weight { get => weight; set => weight = value; }
+
+
+        public Product()
+        {
+            this.name = Name;
+            this.price = Price;
+            this.brand = Brand;
+            this.weight = Weight;
+        }
+    }
+}
---
+++ csvReader/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            readCSVFile();
+            Console.ReadLine();
+        }
+        static void readCSVFile()
+        {
+            string[] csvRows = File.ReadAllLines("products.csv"); //questão a ser pensada - dinamicidade (argumento, chamada direta) 
+
+            List<Product> productList = new List<Product>();
+
+
+            foreach (var line in csvRows)
+            {
+
+                string[] lineValues = line.Split(',');
+
+                Product productObj = new Product() { Name = lineValues[0], Price = lineValues[1], Brand = lineValues[2], Weight = lineValues[3] };
+
+                productList.Add(productObj); //if na product
+            }
+            Console.WriteLine("PRODUCTS");
+
+
+            productList.ForEach(product => Console.WriteLine($"{product.Name} {product.Price} {product.Brand} {product.Weight}")); //pergunt. brand -, op 1 e 2, dar o print da op escolhioda
+
+        }
+    }
+}
